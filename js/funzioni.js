3a93605a825c7e7ab6e0e673748c84bdbc87756e
male-andante <andreadanteprivitera@gmail.com>
2025-02-04 19:24:55 +0100

    fix ultimo funzione 1

--- js/funzioni.js
+++ js/funzioni.js
@@ -6,10 +6,14 @@ let n2 = parseInt(prompt('scrivi un altro numero'))
 const checkInt = function (n1, n2) {
 
 if ((n1 == 50 || n2==50) || (n1+n2 == 50)){
+    console.log('check con successo')
     return true
 } else {
+    console.log('check con insuccesso')
     return false
 }
 }
+
+console.log(checkInt(n1, n2))
 
 // funzione che accetta una stringa e la posizione del carattere e rimuova il carattere dalla stringa. Ritorna la stringa modificata.
