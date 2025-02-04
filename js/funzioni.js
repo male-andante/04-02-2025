fa498084029e8da0a7a7df5c9ba751b68488c6c8
male-andante <andreadanteprivitera@gmail.com>
2025-02-04 20:02:06 +0100

    fatta funzione 3

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,4 +1,4 @@
-// funzione che controlla SE tra 2 numeri interi, 1 dei 2 = 50 || n1 +n2 = 50 e return True se è vero.
+// 1 funzione che controlla SE tra 2 numeri interi, 1 dei 2 = 50 || n1 +n2 = 50 e return True se è vero.
 
 /*let n1 = parseInt(prompt('scrivi un numero'))
 let n2 = parseInt(prompt('scrivi un altro numero'))
@@ -16,15 +16,16 @@ if ((n1 === 50 || n2 === 50) || (n1+n2 === 50)){
 
 console.log(checkInt(n1, n2))*/
 
-// funzione che accetta una stringa e la posizione del carattere e rimuova il carattere dalla stringa. Ritorna la stringa modificata.
+// 2 funzione che accetta una stringa e la posizione del carattere e rimuova il carattere dalla stringa. Ritorna la stringa modificata.
 
-let string = prompt('scrivi una parola')
+/*let string = prompt('scrivi una parola')
 let pos = parseInt(prompt('scegli un indice da rimuovere'))
 
 const modifyString = function (string, pos) {
     
     if (pos <= string.length){
-    let newString = string.slice(0, pos) + string.slice(pos +1)
+    //let newString = string.slice(0, pos) + string.slice(pos +1) alternativa 1 più macchinosa
+    let newString = string.split(pos).join('')  //alternativa 2 più immediata
     console.log(newString)
     return newString
 } else {
@@ -33,5 +34,20 @@ const modifyString = function (string, pos) {
 
 }
 
-console.log(modifyString(string, pos))
+console.log(modifyString(string, pos))*/
+
+// 3 funzione che che controlla 2 numeri: 40<= n1&n2 <= 60 || 70<= n1&n2 <= 100.
+
+let n1 = parseInt(prompt('scegli un numero'))
+let n2 = parseInt(prompt('scegli un altro numero'))
+
+const contained = function (n1,n2) {
+    if ((n1 >= 40 && n2<= 60)||(n1 >= 70 && n2 <= 100)){
+        return true
+    } else {
+        return false
+    }
+}
+
+alert(contained(n1,n2))
 
